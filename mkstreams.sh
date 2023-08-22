7ea8364f0791a67a5b95637758eb5e8b3f302a2c
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-08-22 18:50:27 -0400

    Add `dl` as a function

--- mkstreams.sh
+++ mkstreams.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-STREAMS=$(curl https://leanprover-community.github.io/archive/ | sed '/href="stream/!d' | sed 's/^    <p><a href="/https:\/\/leanprover-community.github.io\/archive\//g' | sed 's/".\+$//g')
-STREAMS=$(echo "$STREAMS" | head -n 1)
+
+dl () {
+  BASEURL=$(echo $1 | sed 's/\/index.html$//g')
+  URLS=$( curl -s $1 \
+          | sed '/href="topic/!d' \
+          | sed 's/^  <li><a href="//g' \
+          | sed 's/".\+$//g' \
+          | sed "s/'/\\\\'/g" \
+          | sed "s,^,$BASEURL/,g")
+  TOPIC=$(echo $1 \
+          | sed 's/^.\+\/stream\/[0-9]\+-//g' \
+          | sed 's/\/[^\/]\+$//g' \
+          | sed 's/-\/-/-/g')
+  mkdir out/$TOPIC
+  cd out/$TOPIC
+  echo "$URLS" | xargs -P 0 -I{} wget {}
+}
+
+STREAMS=$(curl https://leanprover-community.github.io/archive/ \
+          | sed '/href="stream/!d' \
+          | sed 's/^    <p><a href="/https:\/\/leanprover-community.github.io\/archive\//g' \
+          | sed 's/".\+$//g' \
+          | head -n 1)
 rm -rf out
 mkdir out
-xargs -0 -I{} ./dl.sh {} < <(echo "$STREAMS" | tr \\n \\0)
+export -f dl
+xargs -0 -I{} bash -c "dl {}" < <(echo "$STREAMS" | tr \\n \\0)
