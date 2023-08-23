b2d2138a05c7dc53777b4dde671b6ecf7c5bd5e6
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-08-23 10:18:06 -0400

    Remove `dump.sh`, rename `tidy.sh` -> `plain.sh`

--- dump.sh
+++
@@ -1,6 +0,0 @@
-echo "$1"
-BASENAME=$(echo "$1" | sed 's/^.\+\///g' | sed 's/.html$//g' | sed 's/^-\+//g' | sed 's/(//g' | sed 's/)//g')
-FILENAME="txt/$BASENAME.txt"
-lynx --dump $1 > $FILENAME
-sed -i '/https:\/\/leanprover-community\.github\.io\/archive\/stream.\+\.html#[0-9]\+/d' $FILENAME
-sed -i '/Hidden links:/,$d' $FILENAME
