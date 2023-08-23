8ee5583eb3c5102fa46c576670b4dc276dddb7de
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-08-23 10:17:19 -0400

    Use lowercase variable names
    * Fix single quote bug in `tidy.sh`

--- mkstreams.sh
+++ mkstreams.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 dl () {
-  BASEURL=$(echo $1 | sed 's/\/index.html$//g')
-  URLS=$( curl -s $1 \
+  baseurl=$(echo $1 | sed 's/\/index.html$//g')
+  urls=$( curl -s $1 \
           | sed '/href="topic/!d' \
           | sed 's/^  <li><a href="//g' \
           | sed 's/".\+$//g' \
           | sed "s/'/\\\\'/g" \
-          | sed "s,^,$BASEURL/,g")
-  TOPIC=$(echo $1 \
+          | sed "s,^,$baseurl/,g")
+  topic=$(echo $1 \
           | sed 's/^.\+\/stream\/[0-9]\+-//g' \
           | sed 's/\/[^\/]\+$//g' \
           | sed 's/-\/-/-/g')
-  mkdir out/$TOPIC
-  cd out/$TOPIC
-  echo "$URLS" | xargs -P 0 -I{} wget {}
+  mkdir out/$topic
+  cd out/$topic
+  echo "$urls" | xargs -P 0 -I{} wget {}
 }
 
-STREAMS=$(curl https://leanprover-community.github.io/archive/ \
+streams=$(curl https://leanprover-community.github.io/archive/ \
           | sed '/href="stream/!d' \
           | sed 's/^    <p><a href="/https:\/\/leanprover-community.github.io\/archive\//g' \
           | sed 's/".\+$//g' \
@@ -25,4 +25,4 @@ STREAMS=$(curl https://leanprover-community.github.io/archive/ \
 rm -rf out
 mkdir out
 export -f dl
-xargs -0 -I{} bash -c "dl {}" < <(echo "$STREAMS" | tr \\n \\0)
+xargs -0 -I{} bash -c "dl {}" < <(echo "$streams" | tr \\n \\0)
--- tidy.sh
+++ tidy.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
+dump () {
+  basename=$(echo "$1" | sed 's/^.\+\///g' | sed 's/.html$//g' | sed 's/^-\+//g' | sed 's/(//g' | sed 's/)//g')
+  filename="txt/$basename.txt"
+  lynx --dump $1 > $filename
+  sed -i '/https:\/\/leanprover-community\.github\.io\/archive\/stream.\+\.html#[0-9]\+/d' $filename
+  sed -i '/Hidden links:/,$d' $filename
+}
+
 rm -rf txt
 mkdir txt
-SOURCES=$(find -iname "*.html" | sed 's/(/\\\\(/g' | sed 's/)/\\\\)/g')
-echo "$SOURCES"
-xargs -I{} bash -c "./dump.sh {}" < <(echo "$SOURCES")
+sources=$(find -iname "*.html" | sed 's/(/\\\\(/g' | sed 's/)/\\\\)/g' | sed "s,',\\\\\\\\\\\',g")
+export -f dump
+xargs -t -I{} bash -c "dump {}" < <(echo "$sources")
